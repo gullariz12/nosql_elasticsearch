69a07cb31ec1fe4669543ee2a303d8b5591360fa
gullariz <gull.ariz@infinikorn.com>
2022-07-12 00:43:52 +0500

    added raw UI

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,61 @@
+class ListingsController < ApplicationController
+  before_action :set_listing, only: %i[ show edit update destroy ]
+
+  def index
+    if params[:query].present?
+      @listings = Listing.search(params[:query]).records
+    else
+      @listings = Listing.all
+    end
+  end
+
+  def show
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def edit
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+
+    respond_to do |format|
+      if @listing.save
+        format.html { redirect_to listing_url(@listing), notice: "Listing was successfully created." }
+
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @listing.update(listing_params)
+        format.html { redirect_to listing_url(@listing), notice: "Listing was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @listing.destroy
+
+    respond_to do |format|
+      format.html { redirect_to listings_url, notice: "Listing was successfully destroyed." }
+    end
+  end
+
+  private
+    def set_listing
+      @listing = Listing.find(params[:id])
+    end
+
+    def listing_params
+      params.require(:listing).permit(:beds, :baths)
+    end
+end
