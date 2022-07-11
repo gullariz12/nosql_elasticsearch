6a2b5db4e5780d12d03724a1b58ce802fb95223b
gullariz <gull.ariz@infinikorn.com>
2022-07-11 23:40:20 +0500

    added elastic search logic

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,7 +7,41 @@ class Listing
   field :beds, type: Integer
   field :baths, type: Integer
 
-  def as_indexed_json
-    as_json(except: [:id, :_id])
+  index_name 'sample_task'
+  document_type self.name.downcase
+
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: false do
+      indexes :beds, type: :integer
+      indexes :baths, type: :integer
+    end
+  end
+
+  def self.search(query)
+    __elasticsearch__.search(
+      {
+        query:
+        {
+          range: {
+            beds: {
+              gte: query[:beds_min],
+              lte: query[:beds_max],
+              boost: 2.0
+            }
+          },
+          range: {
+            baths: {
+              gte: query[:baths_min],
+              lte: query[:baths_max],
+              boost: 2.0
+            }
+          }
+        }
+      }
+    )
+  end
+
+  def as_indexed_json(options = nil)
+    self.as_json( only: [ :beds, :baths ] )
   end
 end
